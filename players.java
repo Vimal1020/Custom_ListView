40b4c6a23ea602aafaed5213971d5f3212421242
Vimal1020 <50143181+Vimal1020@users.noreply.github.com>
2020-08-18 17:48:19 +0530

    Create players.java

---
+++ players.java
@@ -0,0 +1,23 @@
+package com.example.proj;
+
+public class players{
+    public String name;
+    public double height;
+    public String img;
+    public int age;
+    public long salary;
+    public String nationality;
+
+    public players(String pname, double height, String img, int age, long salary, String nation)
+    {
+        this.name=pname;
+        this.height=height;
+        this.img=img;
+        this.age=age;
+        this.salary=salary;
+        this.nationality=nation;
+    }
+
+
+
+}
